ffbb662fd1085f77c8758624a4140aaa0c77d83c
Nicole Pyzza <nicolepyzza@Nicoles-MacBook-Pro.local>
2024-06-21 21:51:10 -0400

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">sheltr</header>
+      <p className="App-subheader">coming soon</p>
+    </div>
+  );
+}
+
+export default App;
